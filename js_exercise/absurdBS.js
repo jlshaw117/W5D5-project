437d5ee8770f29b4ab6991f3056c81adbffc4505
jlshaw117 <jlshaw117@gmail.com>
2018-11-23 15:49:05 -0800

    Add askIfGraterThan and innerBubbleSortLoop

---
+++ js_exercise/absurdBS.js
@@ -0,0 +1,60 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Write this first.
+function askIfGreaterThan(el1, el2, callback) {
+  // Prompt user to tell us whether el1 > el2; pass true back to the
+  // callback if true; else false.
+  reader.question(`${el1} > ${el2} type true or false.`, function (res) {
+    if (res === 'true') {
+      callback(true);
+    } else {
+      callback(false);
+    }    
+  });
+}
+
+// Once you're done testing askIfGreaterThan with dummy arguments, write this.
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  // Do an "async loop":
+  if (i < arr.length - 1) {
+    askIfGreaterThan(arr[i], arr[i + 1], function (arg) {
+      if (arg === true) {
+        temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        madeAnySwaps = true;
+      } 
+    });
+    innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+  }
+  // 1. If (i == arr.length - 1), call outerBubbleSortLoop, letting it
+  //    know whether any swap was made.
+  // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
+  //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
+  //    continue the inner loop. You'll want to increment i for the
+  //    next call, and possibly switch madeAnySwaps if you did swap.
+}
+// 
+// // Once you're done testing innerBubbleSortLoop, write outerBubbleSortLoop.
+// // Once you're done testing outerBubbleSortLoop, write absurdBubbleSort.
+// 
+// function absurdBubbleSort(arr, sortCompletionCallback) {
+//   function outerBubbleSortLoop(madeAnySwaps) {
+//     // Begin an inner loop if you made any swaps. Otherwise, call
+//     // `sortCompletionCallback`.
+//   }
+// 
+//   // Kick the first outer loop off, starting `madeAnySwaps` as true.
+// }
+
+askIfGreaterThan(5, 10, (arg) => console.log(arg));
+
+// absurdBubbleSort([3, 2, 1], function (arr) {
+//   console.log("Sorted array: " + JSON.stringify(arr));
+//   reader.close();
+// });
